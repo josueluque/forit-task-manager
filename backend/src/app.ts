67070beccacc686af6fad754bfca0be22c8eb67c
Josue Luque <jluqueherbas@gmail.com>
2026-04-07 20:03:37 -0300

    fix: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import tasksRouter from './routes/task'
 import {errorHandler} from './middlewares/errorHandler'
 
@@ -6,6 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api', tasksRouter)
 app.use(errorHandler)
