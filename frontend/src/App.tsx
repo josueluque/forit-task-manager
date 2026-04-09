86f68e25a44cdd7e6df972b0d1e18f0ec0f992c7
Josue Luque <jluqueherbas@gmail.com>
2026-04-09 10:13:15 -0300

    refactor: se mueve listado de tareas a pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import TaskList from './components/TaskList'
+import TaskList from './pages/TaskList'
 
 const App = () => {
   return (
