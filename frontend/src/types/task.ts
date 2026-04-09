8bbccfcd1bb94e142c2ffcb532b30e6198861e63
Josue Luque <jluqueherbas@gmail.com>
2026-04-08 23:43:06 -0300

    add dto createTask y updateTask

--- frontend/src/types/task.ts
+++ frontend/src/types/task.ts
@@ -4,5 +4,15 @@ export interface Task {
     description: string
     completed: boolean
     createdAt: string
-    updatedAt: string
+}
+
+export interface CreateTaskDto {
+  title: string
+  description?: string
+}
+
+export interface UpdateTaskDto {
+  title?: string
+  description?: string
+  completed?: boolean
 }
